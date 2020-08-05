d4343862f7d067d1bc1985f420622c516e3086b8
Vitiokas44 <russ25081989@gmail.com>
2020-08-05 18:48:55 -0500

    add zombieAttack.java

---
+++ src/utility/zombieAttack.java
@@ -0,0 +1,36 @@
+package utility;
+
+public class zombieAttack {
+    public static void main(String[] args) {
+
+            String firstName = "Victor";
+            String lastName = "Russ";
+            char gender = 'M';
+            byte age = 30;
+            String companyName = "NATCO";
+            String jobTitle = "driver";
+            boolean isFullTime = true;
+            boolean isMarried = false;
+            double salary = 110000.55;
+
+            System.out.println("Employee full name is:"+firstName+" "+lastName);
+            System.out.println(firstName+ " "+lastName+" "+"gender is:"+" "+gender);
+            System.out.println(firstName+" "+lastName+" "+"age is:at:"+" "+companyName);
+            System.out.println(firstName+" "+lastName+" "+"jobTitle is:"+" "+jobTitle);
+            System.out.println(firstName+" "+lastName+" "+ "salary is:"+""+salary);
+
+            System.out.println(firstName+" "+lastName+" "+"is full time Employee:"+" "+isFullTime);
+            System.out.println(firstName+" "+lastName+" "+"is married:"+" "+isMarried);
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
