972d26de31d66a23c9027060c7f0c33003fdad85
Vitiokas44 <68917862+Vitiokas44@users.noreply.github.com>
2020-08-05 20:37:34 -0500

    Update ffff.java

--- src/utility/ffff.java
+++ src/utility/ffff.java
@@ -2,7 +2,7 @@ package utility;
 
 public class ffff {
     public static void main(String[] args) {
-        System.out.println("ggggg");
-            System.out.println("hhhhhhhhhhhhg");
+        System.out.println("ggg");
+            System.out.println("hhhg");
     }
 }
